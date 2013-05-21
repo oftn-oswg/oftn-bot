6d6ce0122e8253ae3c5ca31470f3c536ac1663ab
Timo Tijhof <krinklemail@gmail.com>
2013-05-22 01:20:28 +0200

    Add sample-profile.js

---
+++ sample-profile.js
@@ -0,0 +1,14 @@
+module.exports = [{
+	host: 'chat.freenode.net',
+	port: 6667,
+	user: process.env.USER,
+	nick: 'botname',
+	real: 'Description of bot',
+
+	nickserv: 'Example',
+	password: 'password',
+
+	channels: [
+		'#example'
+	]
+}];
